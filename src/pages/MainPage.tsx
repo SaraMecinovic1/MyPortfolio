4d1297629542b8c834fe657eb0eaf62603c5e726
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-02-04 12:15:57 +0100

    start new project

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+const MainPage = () => {
+  return <div className="w-full h-auto ">
+
+    
+  </div>;
+};
+
+export default MainPage;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+  	extend: {
+  		colors: {
+  			primary: '#b1b493',
+  			secondary: '#232931',
+  			textColor: '#c9c7c7 ',
+  			mainText: '#121212'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
