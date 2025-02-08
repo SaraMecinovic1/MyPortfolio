efc28fed938ce1b3e0627b0ce33383ac653ee813
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-02-08 20:57:21 +0100

    added load more button

--- src/component/ProjectsPage/index.tsx
+++ src/component/ProjectsPage/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FilmifyImg from "../../assets/filmify.jpeg";
 import FinsmartImg from "../../assets/finsmart.jpeg";
 import DataImg from "../../assets/data.jpeg";
@@ -32,63 +33,63 @@ const projects = [
     alt: "Data project preview",
     title: "EvoGym Web Site",
   },
-  {
-    id: 5,
-    image: SpotifyImg,
-    alt: "Data project preview",
-    title: "EvoGym Web Site",
-  },
-  {
-    id: 6,
-    image: Todo,
-    alt: "Data project preview",
-    title: "EvoGym Web Site",
-  },
+  { id: 5, image: SpotifyImg, alt: "Spotify clone", title: "Spotify clone" },
+  { id: 6, image: Todo, alt: "To Do App", title: "To Do App" },
   {
     id: 7,
     image: Bella,
     alt: "Data project preview",
-    title: "EvoGym Web Site",
+    title: "Jewerly Web App",
   },
-  {
-    id: 8,
-    image: Nexus,
-    alt: "Data project preview",
-    title: "EvoGym Web Site",
-  },
+  { id: 8, image: Nexus, alt: "Data project preview", title: "Nexus Web Site" },
 ];
 
 const ProjectsPage = () => {
+  const [visibleProjects, setVisibleProjects] = useState(6);
+
+  const loadMore = () => {
+    setVisibleProjects(projects.length);
+  };
+
   return (
     <section id="projects">
-      <div className="w-full h-[100vh] px-10 flex font-inter items-center flex-col gap-3">
-        <h1 className="mt-32 text-3xl md:text-[43px] font-semibold text-primary">
+      <div className="w-full min-h-screen mb-20 px-10 flex font-inter items-center flex-col gap-3">
+        <h1 className="mt-32 text-4xl md:text-[43px] font-semibold text-primary">
           MY PROJECTS
         </h1>
-        <h2 className="text-lg italic">
+        <h2 className="text-lg xl:text-xl italic text-center">
           “Talk is cheap. Show me the code.” – Linus Torvalds
         </h2>
 
-        <div className="w-full h-full  grid grid-cols-4 gap-5 mt-20">
-          {projects.map((project) => (
+        <div className="w-full max-w-7xl mx-auto h-full grid grid-cols-1 md:grid-cols-2  2xl:grid-cols-3 gap-10 mt-20">
+          {projects.slice(0, visibleProjects).map((project) => (
             <div
               key={project.id}
-              className="w-[440px] mb-5  h-[400px] rounded-lg shadow-lg flex flex-col cursor-pointer relative group"
+              className="w-full max-w-[460px] mx-auto mb-5 h-[380px] rounded-lg shadow-lg flex flex-col cursor-pointer relative group"
             >
-              <div className="w-full h-[85%] relative ">
+              <div className="w-full h-[85%] relative">
                 <img
                   src={project.image}
                   alt={project.alt}
-                  className="w-full h-full object-cover rounded-xl"
+                  className="w-full h-full object-cover rounded-lg"
                 />
                 <div className="absolute top-0 left-0 w-full h-full bg-primary opacity-0 flex justify-center items-center text-white font-semibold text-xl rounded-lg group-hover:opacity-95 transition-opacity">
-                  Click for more
+                  Click for more details.
                 </div>
               </div>
               <h3 className="text-xl mt-4 text-center">{project.title}</h3>
             </div>
           ))}
         </div>
+
+        {visibleProjects < projects.length && (
+          <button
+            onClick={loadMore}
+            className="mt-10 px-6 py-2 text-white bg-primary rounded-lg shadow-md hover:bg-opacity-90 transition"
+          >
+            Load More...
+          </button>
+        )}
       </div>
     </section>
   );
