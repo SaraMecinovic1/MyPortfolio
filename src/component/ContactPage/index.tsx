2d2d123e51728b32623de864bb5ec79b7ff6f438
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-02-11 21:04:09 +0100

    ui for contact section is done

--- src/component/ContactPage/index.tsx
+++ src/component/ContactPage/index.tsx
@@ -28,11 +28,11 @@ const ContactPage = () => {
   return (
     <section id="contact">
       <div className="w-full h-auto min-h-[100vh] bg-white px-5 flex flex-col justify-center items-center gap-6 font-inter py-10">
-        <h1 className="text-4xl md:text-[43px] sm:mb-10 font-semibold text-primary">
+        <h1 className="text-4xl md:text-[43px] sm:mb-10 mt-20 sm:mt-0 font-semibold text-primary">
           CONTACT ME
         </h1>
 
-        <div className=" bg-red-100 w-full lg:w-[90%] xl:w-[70%] 2xl:w-[60%] h-auto p-6 md:items-center flex flex-col  lg:flex-row justify-between">
+        <div className=" w-full lg:w-[90%] xl:w-[70%] 2xl:w-[60%] h-auto p-2 md:items-center flex flex-col  lg:flex-row justify-between">
           {/* EMAIL FORM */}
           <div className="w-full md:max-w-[80%]  lg:w-[65%]   h-auto bg-[#f8f9fa] p-5 sm:p-10 rounded-lg shadow-sm">
             <form className="flex flex-col gap-4 w-full">
@@ -72,21 +72,23 @@ const ContactPage = () => {
             </form>
           </div>
           {/* INFO */}
-          <div className="w-full md:w-[60%] lg:w-[30%] h-auto flex flex-col justify-start gap-10 pt-10 items-start">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-6 w-full">
+          <div className="w-full md:w-[80%] md:justify-center  lg:w-[30%] h-auto flex flex-col justify-start gap-10 pt-10 items-start">
+            <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-1 gap-6 w-full">
               {information.map((info, index) => (
                 <div
                   key={index}
                   className="flex flex-row items-center justify-start gap-2  sm:ml-0  text-center"
                 >
-                  <div className="bg-primary rounded-full lg:p-4 p-3 ">
+                  <div className="bg-primary rounded-full lg:p-4 xl:p-5 p-3 ">
                     <img
                       src={info.img}
                       alt={`Topic ${index}`}
-                      className="max-h-[25px] max-w-[25px] lg:h-[20px] lg:w-[20px] object-contain"
+                      className="max-h-[25px] max-w-[25px] lg:h-[20px] lg:w-[20px] xl:h-[25px] xl:w-[25px] object-contain"
                     />
                   </div>
-                  <p className="text-md sm:text-lg text-gray-700">{info.desc}</p>
+                  <p className="text-md sm:text-lg md:mr-5 text-gray-700">
+                    {info.desc}
+                  </p>
                 </div>
               ))}
             </div>
